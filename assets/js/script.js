b0e168cccdd1819e8c0e20b404db4e5e68098329
AjayNaruka <naruka.ajay1997@gmail.com>
2021-04-06 20:17:21 +0200

    fine

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+var username;
+var surname;
+var favColor;
+
+username = prompt("Name");
+surname = prompt("Surname");
+favColor = prompt("Favorite Color");
+
+document.getElementById("password").innerHTML = username+surname+favColor+"21";
